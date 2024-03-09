489816bac1ec95fb5c77a4749ec9af0b8d6316e9
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-03-09 16:48:58 +0100

    🚧

---
+++ components/HomeCard.tsx
@@ -0,0 +1,25 @@
+import { Group, Paper, Text } from "@mantine/core";
+
+interface HomeCardProps {
+  title: string | React.ReactNode;
+  icon?: React.ReactNode;
+  description: string | React.ReactNode;
+  children?: React.ReactNode;
+}
+
+export const HomeCard: React.FC<HomeCardProps> = ({ title, description, children, icon }) => {
+  return (
+    <Paper withBorder shadow="md" radius={"md"} p={"md"}>
+      <Group>
+        {icon}
+        <Text variant="gradient" fz={24} fw={700}>
+          {title}
+        </Text>
+      </Group>
+      <Text fz={16} fw={400}>
+        {description}
+      </Text>
+      {children}
+    </Paper>
+  );
+};
--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -1,5 +1,19 @@
-import { Button, Container, Group, Image, Stack, Text } from "@mantine/core";
+import { Button, Container, Divider, Group, Image, SimpleGrid, Stack, Text } from "@mantine/core";
+import {
+  IconApi,
+  IconBrandCakephp,
+  IconBrandJavascript,
+  IconBrandLaravel,
+  IconBrandWordpress,
+  IconCode,
+  IconDatabase,
+  IconMenuDeep,
+  IconPaperclip,
+  IconSettings2,
+  IconTerminal,
+} from "@tabler/icons-react";
 import NextImage from "next/image";
+import { HomeCard } from "./HomeCard";
 import phpBones from "./image.png";
 
 export default function HomeContent() {
@@ -25,6 +39,77 @@ export default function HomeContent() {
             Get started →
           </Button>
         </Group>
+
+        <Divider my="xs" label={<Text fz={24}>features</Text>} labelPosition="center" />
+
+        <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
+          <HomeCard
+            title="Laravel-like"
+            icon={<IconBrandLaravel />}
+            description="A revolutionary framework that combines Composer, npm, Laravel coding style, and Gulp to bring modern development tools to the WordPress environment"
+          />
+          <HomeCard
+            title="Console Commands"
+            icon={<IconTerminal />}
+            description="Bones command-line interface for developing WordPress plugins, providing helpful commands and customization options, while also simplifying the deployment process"
+          />
+
+          <HomeCard
+            title="Menu"
+            icon={<IconMenuDeep />}
+            description="Offers a unique approach to menus routing, form method spoofing, and redirects in load, making it a powerful tool for building custom WordPress admin menu"
+          />
+
+          <HomeCard
+            title="Composer Support"
+            icon={<IconBrandCakephp />}
+            description="WP Bones supports Composer for dependency management."
+          />
+
+          <HomeCard
+            title="Options"
+            icon={<IconSettings2 />}
+            description="The plugin options system in WordPress is efficient and easy to use, allowing for easy storage and retrieval of plugin settings"
+          />
+
+          <HomeCard
+            title="Pages"
+            icon={<IconPaperclip />}
+            description="Creating custom page routes and pages in Nextra is straightforward and flexible, allowing for easy navigation and page creation without menus"
+          />
+          <HomeCard
+            title="Logging"
+            icon={<IconPaperclip />}
+            description="WPBones makes it easy and beautiful to log messages in your WordPress plugin with its configuration options and helper functions"
+          />
+          <HomeCard
+            title="Custom Post"
+            icon={<IconBrandWordpress />}
+            description="You can easily create a custom post type and custom taxonomy service provider"
+          />
+          <HomeCard
+            title="Rest API"
+            icon={<IconApi />}
+            description="Provides a simple and efficient way to handle the WordPress REST API, including authentication and customizable routes"
+          />
+          <HomeCard
+            title="Shortcodes"
+            icon={<IconCode />}
+            description="A powerful and easy way to add custom functionality to WordPress. Simply add your shortcode classes to the config file and let the magic begin!"
+          />
+          <HomeCard
+            title="Ajax Calls"
+            icon={<IconBrandJavascript />}
+            description="Add support for Ajax calls in WordPress with three different access levels: for logged-in users, not-logged-in users, and everyone"
+          />
+          <HomeCard
+            title="ORM"
+            icon={<IconDatabase />}
+            description="Integrate Illuminate Eloquent ORM into WP Bones, allowing for easy database querying and model creation"
+          />
+        </SimpleGrid>
+
+        <Divider my="xs" label={<Text fz={24}>used by</Text>} labelPosition="center" />
       </Stack>
     </Container>
   );
