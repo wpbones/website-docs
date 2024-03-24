5f4c1b0ef37c6c2039014186b3442cabdbf425ed
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-03-24 15:36:03 +0100

    ::wip

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -3,24 +3,31 @@
 import { Button, Container, Grid, Group, Image, SimpleGrid, Stack, Text } from "@mantine/core";
 import {
   IconApi,
-  IconBrandCakephp,
   IconBrandJavascript,
   IconBrandLaravel,
   IconBrandReact,
   IconBrandWordpress,
   IconCode,
   IconDatabase,
+  IconEyeStar,
   IconMenuDeep,
   IconPaperclip,
+  IconRectangle,
   IconSettings2,
   IconTerminal,
 } from "@tabler/icons-react";
 import NextImage from "next/image";
 import { HomeCard } from "./HomeCard";
+import ajaxImage from "./ajax.png";
+import apiImage from "./api.png";
 import consoleImage from "./console.png";
 import controllerImage from "./controller.png";
+import cptImage from "./cpt.png";
+import dbImage from "./db.png";
 import phpBones from "./image.png";
+import logsImage from "./logs.png";
 import menuImage from "./menu.png";
+import migrationImage from "./migration.png";
 import optionsImage from "./option.png";
 import pageImage from "./page.png";
 import reactImage from "./react-js.png";
@@ -71,6 +78,7 @@ export default function HomeContent() {
           />
           <HomeCard
             title="Console Commands"
+            href="/docs/BonesConsole/bones-console"
             icon={<IconTerminal />}
             image={consoleImage}
             description="Bones command-line interface for developing WordPress plugins, providing helpful commands and customization options, while also simplifying the deployment process.
@@ -78,68 +86,102 @@ export default function HomeContent() {
           />
           <HomeCard
             title="ReactJS"
+            href="/docs/GettingStarted/assets"
             icon={<IconBrandReact />}
             image={reactImage}
             description="WP Bones provides a simple and efficient way to integrate ReactJS into your WordPress plugin, allowing for easy creation of custom components and pages. Supports JavaScript and TypeScript. Styles are handled by Less and Sass."
           />
 
           <HomeCard
+            title="ORM"
+            href="/docs/DatabaseORM/query-builder"
+            icon={<IconDatabase />}
+            image={dbImage}
+            description="Integrate Illuminate Eloquent ORM into WP Bones, allowing for easy database querying and model creation."
+          />
+
+          <HomeCard
+            title="Migration and seeding"
+            href="/docs/DatabaseORM/migrations"
+            icon={<IconDatabase />}
+            image={migrationImage}
+            description="Supports database migration and seeding, allowing for easy database management and version control. You can create and run migrations and seeders with ease."
+          />
+
+          <HomeCard
             title="Menu"
+            href="/docs/CoreConcepts/menus"
             icon={<IconMenuDeep />}
             image={menuImage}
-            href="/docs/CoreConcepts/menus"
             description="Offers a unique approach to menus routing, form method spoofing, and redirects in load, making it a powerful tool for building custom WordPress admin menu."
           />
 
           <HomeCard
             title="Pages"
+            href="/docs/CoreConcepts/pages-routing"
             icon={<IconPaperclip />}
             image={pageImage}
             description="Creating custom page routes and pages is straightforward and flexible, allowing for easy navigation and page creation without menus."
           />
+          <HomeCard
+            title="Logging"
+            href="/docs/CoreConcepts/logging"
+            icon={<IconPaperclip />}
+            image={logsImage}
+            description="WPBones makes it easy and beautiful to log messages in your WordPress plugin with its configuration options and helper functions"
+          />
 
           <HomeCard
-            title="Composer Support"
-            icon={<IconBrandCakephp />}
-            description="WP Bones supports Composer for dependency management."
+            title="Ajax Calls"
+            href="/docs/ServicesProvider/ajax"
+            icon={<IconBrandJavascript />}
+            image={ajaxImage}
+            description="Add support for Ajax calls in WordPress with three different access levels: for logged-in users, not-logged-in users, and everyone"
+          />
+
+          <HomeCard
+            title="Custom Post and Taxonomy"
+            href="/docs/ServicesProvider/custom-post-types"
+            icon={<IconBrandWordpress />}
+            image={cptImage}
+            description="You can easily create a custom post type and custom taxonomy service provider"
+          />
+
+          <HomeCard
+            title="Rest API"
+            href="/docs/ServicesProvider/rest-api"
+            icon={<IconApi />}
+            image={apiImage}
+            description="Provides a simple and efficient way to handle the WordPress REST API, including authentication and customizable routes"
           />
 
           <HomeCard
             title="Options"
+            href="/docs/CoreConcepts/options"
             icon={<IconSettings2 />}
             image={optionsImage}
             description="The plugin options system in WordPress is efficient and easy to use, allowing for easy storage and retrieval of plugin settings"
           />
 
           <HomeCard
-            title="Logging"
-            icon={<IconPaperclip />}
-            description="WPBones makes it easy and beautiful to log messages in your WordPress plugin with its configuration options and helper functions"
-          />
-          <HomeCard
-            title="Custom Post"
-            icon={<IconBrandWordpress />}
-            description="You can easily create a custom post type and custom taxonomy service provider"
-          />
-          <HomeCard
-            title="Rest API"
-            icon={<IconApi />}
-            description="Provides a simple and efficient way to handle the WordPress REST API, including authentication and customizable routes"
-          />
-          <HomeCard
             title="Shortcodes"
+            href="/docs/ServicesProvider/shortcodes"
             icon={<IconCode />}
             description="A powerful and easy way to add custom functionality to WordPress. Simply add your shortcode classes to the config file and let the magic begin!"
           />
+
           <HomeCard
-            title="Ajax Calls"
-            icon={<IconBrandJavascript />}
-            description="Add support for Ajax calls in WordPress with three different access levels: for logged-in users, not-logged-in users, and everyone"
+            title="Widgets"
+            href="/docs/ServicesProvider/widgets"
+            icon={<IconRectangle />}
+            description="Widgets are a powerful way to add custom functionality to WordPress. Simply add your widget classes to the config file and let the magic begin!"
           />
+
           <HomeCard
-            title="ORM"
-            icon={<IconDatabase />}
-            description="Integrate Illuminate Eloquent ORM into WP Bones, allowing for easy database querying and model creation"
+            title="And more..."
+            href="/docs"
+            icon={<IconEyeStar />}
+            description="Supports Composer for dependency management, easy plugin activation and deactivation, awesome options system, custom post types, REST API, and shortcodes."
           />
         </SimpleGrid>
       </Stack>
