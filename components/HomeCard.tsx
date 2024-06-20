cbdad9f208133a308a1c6d30aecc407f3142cb19
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-06-20 15:31:44 +0200

    🎨 home card

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -2,41 +2,50 @@
 
 import { Group, Image, Paper, Text } from "@mantine/core";
 import NextImage from "next/image";
+import { forwardRef } from "react";
 import classes from "./HomeCard.module.css";
+
 interface HomeCardProps {
   title: string | React.ReactNode;
   icon?: React.ReactNode;
   description: string | React.ReactNode;
   children?: React.ReactNode;
-  image?: any;
+  image?: string;
   href?: string;
 }
 
 /**
- * HomeCard component props.
+ * HomeCard component forwardRef.
  *
- * @param title - The title of the card.
- * @param description - The description text in the card.
- * @param children - Any additional content inside the card.
- * @param icon - An optional icon to display in the card header.
- * @param image - An optional image to display in the card.
- * @param href - An optional href if the card should link somewhere.
+ * @param props - The props for the HomeCard component.
+ * @param ref - The ref for forwarding.
  */
-export const HomeCard: React.FC<HomeCardProps> = ({ title, description, children, icon, image, href }) => {
-  return (
-    <Paper component="a" href={href} withBorder shadow="md" radius={"lg"} p={"md"} className={classes.card}>
-      <Group>
-        {icon}
-        <Text fz={24} fw={700}>
-          {title}
+export const HomeCard = forwardRef<HTMLAnchorElement, HomeCardProps>(
+  ({ title, description, children, icon, image, href, ...others }, ref) => {
+    return (
+      <Paper
+        component="a"
+        href={href}
+        ref={ref}
+        withBorder
+        shadow="md"
+        radius={"lg"}
+        p={"md"}
+        className={classes.card}
+        {...others}>
+        <Group>
+          {icon}
+          <Text fz={24} fw={700}>
+            {title}
+          </Text>
+        </Group>
+        {image && <Image component={NextImage} radius="md" w="100%" fit="contain" src={image} alt="Card image" />}
+        <Text c="dimmed" fz={16} fw={400}>
+          {description}
         </Text>
-      </Group>
-      {image && <Image component={NextImage} radius="md" w="100%" fit="contain" src={image} alt="PHP Bones" />}
-      <Text c="dimmed" fz={16} fw={400}>
-        {description}
-      </Text>
 
-      {children}
-    </Paper>
-  );
-};
+        {children}
+      </Paper>
+    );
+  },
+);
