f75dfa12f82bec74c00bbdc59d7ca30b910e2ebb
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-10-29 16:49:33 +0100

    🎨 reviews and improves doc about services providers

--- pages/docs/ServicesProvider/_meta.ts
+++ pages/docs/ServicesProvider/_meta.ts
@@ -1,11 +1,11 @@
 export default {
   services: "Services",
+  "autoload-service-providers": "Autoload Service Providers",
   ajax: "AJAX",
   "custom-post-types": "Custom Post Types",
   "custom-taxonomy-types": "Custom Taxonomy Types",
   schedule: "Schedule",
   shortcodes: "Shortcodes",
   widgets: "Widgets",
-  "autoload-service-providers": "Autoload Service Providers",
   "rest-api": "REST API",
 };
